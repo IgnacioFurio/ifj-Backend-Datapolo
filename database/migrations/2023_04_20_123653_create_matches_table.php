dec3729e727f5ce4297e71a6e8c98d1b65c1732b
Ignacio Furió José <bichoifj@gmail.com>
2023-04-20 14:43:26 +0200

    feat: add migration and model for Matches table

---
+++ database/migrations/2023_04_20_123653_create_matches_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('matches', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('season_id');
+            $table->foreign('season_id')
+                ->references('id')
+                ->on('seasons')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            
+            $table->unsignedBigInteger('my_team_id');
+            $table->foreign('my_team_id')
+                ->references('id')
+                ->on('teams')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            
+            $table->unsignedBigInteger('my_rival_id');
+            $table->foreign('my_rival_id')
+                ->references('id')
+                ->on('teams')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+            $table->boolean('locale');
+
+            $table->boolean('friendly');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('matches');
+    }
+};
